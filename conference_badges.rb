373e9d7bb65616d5a0e2c11fc2b28de9060ca5fa
Brennan Roel <brennanroel@gmail.com>
2020-02-19 02:15:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,4 +15,10 @@ def assign_rooms(speakers)
 end
 
 def printer (attendees)
-  
+  batch_badge_creator(attendees).each do |person|
+    puts person
+  end
+  assign_rooms(attendees).each do |person|
+    puts person 
+  end
+end
