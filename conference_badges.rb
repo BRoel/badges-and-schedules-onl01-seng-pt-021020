b12338a3b37b68d358b27625ac81649fb9df57a9
Brennan Roel <brennanroel@gmail.com>
2020-02-19 02:20:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  speakers.map do |name, index|
+  speakers.map.with_index do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
