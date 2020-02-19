abd4fa3e39c3837d24a66535bc949f7c6e1a220c
Brennan Roel <brennanroel@gmail.com>
2020-02-19 01:52:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,6 @@ def badge_maker(name)
  return "Hello, my name is #{name}."
 end
 
-batch_badge_creator ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator (attendees)
+  array ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
